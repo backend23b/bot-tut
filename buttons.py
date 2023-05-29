a4f5b7665b89f3963669ec86aa1a4f0876c9436f
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-29 18:24:21 +0500

    btns

--- buttons.py
+++ buttons.py
@@ -3,15 +3,22 @@ import requests
 TOKEN = '6203711973:AAGcHxZWSz5rbihY3ygUdZpMZeSMxf9qSIc'
 chat_id = '1258594598'
 
+
 url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+
+btn1 = {'text': '12'}
+btn2 = {'text': '13'}
+btn3 = {'text': 'Home'}
+
 
 data = {
     'chat_id': chat_id,
     'text': 'Hello world',
     'reply_markup': {
         'keyboard': [
-            ['Hello', 'World'],
-            ['How are you?'],
+            [btn1, btn2],
+            [btn3],
         ],
         'resize_keyboard': True
     }
