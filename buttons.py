483b53c716c36ff2b46765f32970ea51f38662fa
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-29 17:45:36 +0500

    send keyboarf

---
+++ buttons.py
@@ -0,0 +1,19 @@
+import requests
+
+TOKEN = '6203711973:AAGcHxZWSz5rbihY3ygUdZpMZeSMxf9qSIc'
+chat_id = '1258594598'
+
+url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+data = {
+    'chat_id': chat_id,
+    'text': 'Hello world',
+    'reply_markup': {
+        'keyboard': [
+            ['Hello', 'World'],
+            ['How are you?'],
+        ],
+    }
+}
+
+requests.get(url=url, json=data)
